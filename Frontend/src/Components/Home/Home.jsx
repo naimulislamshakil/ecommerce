9b80174f550310f1062548bd284c13705696c113
Naimul Islam <ms0413354@gmail.com>
2025-09-07 07:29:08 +0600

    try

--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Topbar from '../Common/Topbar';
 import Banner from './Banner';
+import TopProduct from './TopProduct';
 
 const Home = () => {
 	return (
 		<div>
 			<Topbar />
 			<Banner />
+			<TopProduct/>
 		</div>
 	);
 };
---
+++ Frontend/src/Components/Home/TopProduct.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TopProduct = () => {
+	return <div>TopProduct</div>;
+};
+
+export default TopProduct;
