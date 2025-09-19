d422e1351ff28dc98b8d83f04adad0c6e536f624
Naimul Islam <ms0413354@gmail.com>
2025-09-20 00:39:15 +0600

    add backend file with database

---
+++ Backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+// Database
+mongoose
+	.connect('mongodb://0.0.0.0:27017/mydb', {})
+	.then(() => console.log('Database connected successfully.'))
+	.catch((err) => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+	res.send('<h1>How are you?</h1>');
+});
+
+// Start server
+app.listen(PORT, () => {
+	console.log(`Server is running on port: ${PORT}`);
+});
